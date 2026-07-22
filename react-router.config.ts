90163f02d342643e1e9dbcf65f98665f5d67f508
ceenobi <charlesmutob@gmail.com>
2026-07-22 22:56:10 +0100

    perf: prerender static pages (privacy, terms, guide, delete-account-confirmation)

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,6 +5,7 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  prerender: ["/privacy", "/terms", "/support/guide", "/delete-account-confirmation"],
   presets: [vercelPreset()],
   future: {
     v8_middleware: true,
